991e71a48b94f16bfc956ce9ff697f0ca7cd25fa
artur <arturhpiva@hotmail.com>
2016-10-17 16:25:41 -0700

    robot 1

---
+++ p5-zip/empty-example/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+
+	createCanvas(400,400);
+	background(200);
+
+}
+
+function draw() {
+noStroke();
+fill(255,204,100);
+rect(100,200,70,100);
+fill('red');
+rect(75, 220, 25, 45);
+rect(170,220,25,45)
+fill(14, 123, 163);
+rect(115,159,40,40);
+fill(6, 158, 11);
+rect(110,300, 20, 40);
+rect(140,300,20,40);
+fill(0,0,0);
+ellipse(146,170,7,7);
+ellipse(126,170,7,7);
+arc(135,178,30,30,0,PI);
+fill(226, 226, 6);
+
+endShape(CLOSE);
+}
+
+
+
